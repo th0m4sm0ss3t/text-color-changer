819d18c1cc8dfcc57c81d158f6d5dd1c08fd7e10
Thomas MOSSET <61469301+th0m4sm0ss3t@users.noreply.github.com>
2020-09-02 14:48:29 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+let cl1 = document.querySelector('#cl1');
+cl1.addEventListener('click', colorChangerCl1);
+
+let cl2 = document.querySelector('#cl2');
+cl2.addEventListener('click', colorChangerCl2);
+
+let cl3 = document.querySelector('#cl3');
+cl3.addEventListener('click', colorChangerCl3);
+
+let cl4 = document.querySelector('#cl4');
+cl4.addEventListener('click', colorChangerCl4);
+
+let cl5 = document.querySelector('#cl5');
+cl5.addEventListener('click', colorChangerCl5);
+
+let cl6 = document.querySelector('#cl6');
+cl6.addEventListener('click', colorChangerCl6);
+
+let cl7 = document.querySelector('#cl7');
+cl7.addEventListener('click', colorChangerCl7);
+
+let cl8 = document.querySelector('#cl8');
+cl8.addEventListener('click', colorChangerCl8);
+
+let cl9 = document.querySelector('#cl9');
+cl9.addEventListener('click', colorChangerCl9);
+
+let cl10 = document.querySelector('#cl10');
+cl10.addEventListener('click', colorChangerCl10);
+
+let text = document.querySelector('#text');
+
+function colorChangerCl1(){ 
+    text.style.color = '#004369';
+}
+function colorChangerCl2(){ 
+    text.style.color = '#FAD02C';
+}
+function colorChangerCl3(){ 
+    text.style.color = '#E6C2BF';
+}
+function colorChangerCl4(){ 
+    text.style.color = '#282120';
+}
+function colorChangerCl5(){ 
+    text.style.color = '#870A30';
+}
+function colorChangerCl6(){ 
+    text.style.color = '#116530';
+}
+function colorChangerCl7(){ 
+    text.style.color = '#340744';
+}
+function colorChangerCl8(){ 
+    text.style.color = '#FF4500';
+}
+function colorChangerCl9(){ 
+    text.style.color = '#4F0000';
+}
+function colorChangerCl10(){ 
+    text.style.color = '#94C973';
+}
